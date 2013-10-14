a09ec2d607704e268f52de9632dfbc91c424e055
Zach Thompson <escapetherobots@gmail.com>
2013-10-14 15:03:07 -0600

    Initial Commit for Dev02 Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dev02::Application.config.secret_token = '41365745dd5d16f401cd62729b1d6e6884aef99f67c42ed5f4eb8e69426f0679dcc8923f33956fc054b87879c3ff285cf226f2f123b533314769eaecb7f8207b'
